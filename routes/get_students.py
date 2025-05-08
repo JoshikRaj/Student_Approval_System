c6adc03334af06e87886eecb73a630f46d849b4e
Joshik Raj <joshikrajjojo@gmail.com>
2025-05-08 12:00:53 +0530

    added query param for college

--- routes/get_students.py
+++ routes/get_students.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request
 from models import Student, AdmissionOutcome, Recommender
 from sqlalchemy.orm import joinedload
+from sqlalchemy import and_
 
-# Blueprint
 get_students_bp = Blueprint('get_students', __name__)
 
 @get_students_bp.route('/api/students', methods=['GET'])
@@ -10,12 +10,15 @@ def get_students():
     print("Route /api/students accessed")
 
     status_filter = request.args.get('status')
-    print(status_filter)
+    college_filter = request.args.get('college')
 
     query = Student.query.options(joinedload(Student.recommenders))
 
     if status_filter:
         query = query.join(AdmissionOutcome).filter(AdmissionOutcome.status == status_filter)
+
+    if college_filter:
+        query = query.filter(Student.college == college_filter)
 
     students = query.all()
 
@@ -27,14 +30,14 @@ def get_students():
         'email': student.email,
         'aadhar_number': student.aadhar_number,
         'community': student.community,
-        'college':student.college,
+        'college': student.college,
         'branch_1': student.branch_1,
         'branch_2': student.branch_2,
         'branch_3': student.branch_3,
         'board': student.board,
         'twelfth_mark': student.twelfth_mark,
-        'markpercentage':student.markpercentage,
-        'engineering_cutoff': student.engineering_cutoff,
+        'markpercentage': student.markpercentage,
+        'engineering_cutoff': student.engineering_cutoff if student.college == 'TCE' else None,
         'date_of_application': student.date_of_application.strftime('%Y-%m-%d'),
         'year_of_passing': student.year_of_passing,
         'recommenders': [
