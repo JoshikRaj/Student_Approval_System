c619cb6b4cfec7c43a3116ef258dd8ed80fe1f27
vishwarockzzz <80967355+vishwarockzzz@users.noreply.github.com>
2025-05-09 13:27:54 +0530

    Delete college_admission_app directory

--- college_admission_app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
