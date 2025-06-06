25378a865416532a86f584894eb9f89340dba0cb
Joshik Raj <joshikrajjojo@gmail.com>
2025-06-06 12:41:47 +0530

    added final counts for admission outcomes

--- routes/status_tcarts.py
+++ routes/status_tcarts.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
-from models import db, TcartsCourseStatus
+from sqlalchemy import func
+from models import db, TcartsCourseStatus, TcartsAdmissionOutcome
 
 tcarts_status_get_bp = Blueprint('tcarts_status_get', __name__, url_prefix='/api/tcarts')
 
@@ -7,6 +8,19 @@ tcarts_status_get_bp = Blueprint('tcarts_status_get', __name__, url_prefix='/api
 def get_tcarts_status_details():
     # Get all course statuses from tcarts tables
     statuses = TcartsCourseStatus.query.all()
+    outcome_counts_query = (
+        db.session.query(TcartsAdmissionOutcome.status, func.count(TcartsAdmissionOutcome.id))
+        .group_by(TcartsAdmissionOutcome.status)
+        .all()
+    )
+
+    counts_dict = {status: count for status, count in outcome_counts_query}
+    final_counts = {
+        "approved": counts_dict.get("APPROVED", 0),
+        "unallocated": counts_dict.get("UNALLOCATED", 0),
+        "declined": counts_dict.get("DECLINED", 0),
+        "onhold": counts_dict.get("ONHOLD", 0)
+    }
 
     result = []
     total_total_seats = 0
@@ -35,6 +49,15 @@ def get_tcarts_status_details():
         "total_seats": total_total_seats,
         "allocated_seats": total_allocated_seats,
         "remaining_seats": total_remaining_seats
+    })
+    
+    total_count=final_counts["approved"]+final_counts["unallocated"]+final_counts["declined"]+final_counts["onhold"]
+    result.append({
+        "course": "Total Applications",
+        "course_type": "Received",
+        "total_seats": total_count ,
+        "allocated_seats": final_counts["approved"],
+        "remaining_seats": total_count-final_counts["approved"]
     })
 
     return jsonify(result), 200
