fe96a82b71020871e591e6569a12feccde7c40f0
Valliammai22 <swethavalliammai55@gmail.com>
2026-05-05 22:44:59 +0530

    comment db backup code

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from routes.logout import logout_bp
 import os
 import shutil
 from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.background import BackgroundScheduler
 
 import os
 app = Flask(
@@ -92,24 +92,24 @@ def debug_seats():
     return '<br>'.join(out)
 
 
-def backup_database():
-    db_path = os.getenv('DB_PATH') or 'students.db'
-    backup_dir = 'db_backups'
-    os.makedirs(backup_dir, exist_ok=True)
-    backup_path = os.path.join(backup_dir, 'students_backup.db')
+# def backup_database():
+#     db_path = os.getenv('DB_PATH') or 'students.db'
+#     backup_dir = 'db_backups'
+#     os.makedirs(backup_dir, exist_ok=True)
+#     backup_path = os.path.join(backup_dir, 'students_backup.db')
 
-    if os.path.exists(backup_path):
-        os.remove(backup_path)
-    shutil.copy2(db_path, backup_path)
-    print(f"Database backup created at {backup_path} on {datetime.now()}")
+#     if os.path.exists(backup_path):
+#         os.remove(backup_path)
+#     shutil.copy2(db_path, backup_path)
+#     print(f"Database backup created at {backup_path} on {datetime.now()}")
 
-def start_backup_scheduler():
-    from flask import current_app
-    if os.getenv('ENV') == 'production':
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(backup_database, 'interval', days=1, next_run_time=datetime.now())
-        scheduler.start()
-        print("Backup scheduler started for production environment.")
+# def start_backup_scheduler():
+#     from flask import current_app
+#     if os.getenv('ENV') == 'production':
+#         scheduler = BackgroundScheduler()
+#         scheduler.add_job(backup_database, 'interval', days=1, next_run_time=datetime.now())
+#         scheduler.start()
+#         print("Backup scheduler started for production environment.")
 
 
 if __name__ == '__main__':
